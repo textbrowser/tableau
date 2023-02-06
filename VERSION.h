073b984a7130408b74287b8a43a572a6cba13695
textbrowser <textbrowser@gmail.com>
2023-02-05 20:41:54 -0500

    New version.

--- VERSION.h
+++ VERSION.h
@@ -1,7 +1,7 @@
 #ifndef _tableau_version_h_
 #define _tableau_version_h_
 
-#define TABLEAU_CALENDAR_VERSION_STRING "2022.12.25"
+#define TABLEAU_CALENDAR_VERSION_STRING "2023.06.06"
 #define TABLEAU_SEMANTIC_VERSION_STRING "1.0.1"
 
 #endif
